c10c877c3b4df314089700cdc27d2b01004fb2f5
Diego Quintela <diegolucasq@gmail.com>
2022-02-23 11:31:13 -0500

    fixed a bug where the UI wasn't beeing updated with useState

--- client/src/holdings/components/AddTransactions.js
+++ client/src/holdings/components/AddTransactions.js
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { transactionActions } from '../../store/transactionSlice';
 
 // TODO: add condition: buy or sell
 const AddTransactions = (props) => {
+  let transaction;
   const dispatch = useDispatch();
   const showHoldings = useSelector((state) => state.transaction.holdings);
+  console.log(showHoldings);
 
   const [enteredTicker, setEnteredTicker] = useState('');
   const [enteredPrice, setEnteredPrice] = useState('');
   const [enteredQuantity, setEnteredQuantity] = useState('');
-  const [holdings, setHoldings] = useState([]);
+  const [holdings, setHoldings] = useState(showHoldings);
+
+  const tickerInputRef = useRef();
+  const priceInputRef = useRef();
+  const quantityInputRef = useRef();
 
   const submitHandler = (e) => {
     e.preventDefault();
 
-    const transaction = {
-      ticker: enteredTicker.toUpperCase().trim(),
-      price: enteredPrice,
-      quantity: enteredQuantity,
+    transaction = {
+      ticker: tickerInputRef.current.value.toUpperCase().trim(),
+      price: priceInputRef.current.value,
+      quantity: quantityInputRef.current.value,
     };
 
     console.log(transaction);
 
-    setHoldings(showHoldings);
     console.log(holdings);
 
     dispatch(transactionActions.addTransaction(transaction));
   };
+
+  useEffect(() => {
+    setHoldings(showHoldings);
+  }, [showHoldings]);
 
   return (
     <div>
@@ -36,34 +45,40 @@ const AddTransactions = (props) => {
       <form onSubmit={submitHandler}>
         <label htmlFor="text">Company Ticker: </label>
         <input
+          ref={tickerInputRef}
           type="text"
-          value={enteredTicker}
+          //value={enteredTicker}
           placeholder="Ticker"
-          onChange={(e) => setEnteredTicker(e.target.value)}
+          //onChange={(e) => setEnteredTicker(e.target.value)}
         />
 
         <label htmlFor="price">Price</label>
         <input
+          ref={priceInputRef}
           type="number"
-          value={enteredPrice}
+          //value={enteredPrice}
           placeholder="Price"
-          onChange={(e) => setEnteredPrice(e.target.value)}
+          //onChange={(e) => setEnteredPrice(e.target.value)}
         />
 
         <label htmlFor="quantity">Quantity</label>
         <input
+          ref={quantityInputRef}
           type="number"
-          value={enteredQuantity}
+          //value={enteredQuantity}
           placeholder="Shares/Quantity"
-          onChange={(e) => setEnteredQuantity(e.target.value)}
+          // onChange={(e) => setEnteredQuantity(e.target.value)}
         />
 
         <button>Add Transactions</button>
       </form>
       <ul>
-        {holdings.map((item) => (
-          <li>{`${item.ticker} $${item.price} ${item.quantity} shares`}</li>
-        ))}
+        {holdings.length > 0 &&
+          holdings.map((item) => (
+            <li
+              key={item.ticker}
+            >{`${item.ticker} $${item.price} ${item.quantity} shares`}</li>
+          ))}
       </ul>
     </div>
   );
