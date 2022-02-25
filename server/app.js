f3b15336189aee19e4b66c4799abde19637e039d
Diego Quintela <diegolucasq@gmail.com>
2022-02-25 10:35:25 -0500

    Handling unsuported routes

--- server/app.js
+++ server/app.js
@@ -3,11 +3,19 @@ const bodyParser = require('body-parser');
 
 const transactionsRoutes = require('./routes/transactions-routes');
 const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/users', usersRoutes);
 app.use('/api/transactions', transactionsRoutes);
+
+// Unsupported routes
+app.use((req, res, next) => {
+  next(new HttpError('Could not find this route.', 404));
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
