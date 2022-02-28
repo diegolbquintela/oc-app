33a318f143aaed1646f1486cd1737e92d13f1f3a
Diego Quintela <diegolucasq@gmail.com>
2022-02-28 11:02:19 -0500

    Holdings List added to the Holdings page component

--- client/src/holdings/pages/Holdings.js
+++ client/src/holdings/pages/Holdings.js
@@ -1,9 +1,11 @@
 import AddTransactions from '../components/AddTransactions';
+import HoldingsList from '../components/HoldingsList';
 
 const Holdings = () => {
   return (
     <>
       <AddTransactions />
+      <HoldingsList />
     </>
   );
 };
