840860e7c1c590afb3bac6d8a36fc2539149d798
Diego Quintela <diegolucasq@gmail.com>
2022-03-02 11:44:13 -0500

    Style updates and beginning of implementation of a reusable form

--- client/src/holdings/components/AddTransactions.js
+++ client/src/holdings/components/AddTransactions.js
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 
 import classes from './AddTransactions.module.css';
 import Button from '../../shared/components/button/Button';
+import Form from '../../shared/components/form/Form';
 
 // TODO: add condition: buy or sell
 const AddTransactions = (props) => {
@@ -37,28 +38,44 @@ const AddTransactions = (props) => {
 
   return (
     <div>
-      <form onSubmit={submitHandler} ref={formRef}>
-        <div className={classes.form}>
-          <div className={classes.form_fields}>
-            <label htmlFor="text">Company Ticker </label>
-            <input ref={tickerInputRef} type="text" placeholder="Ticker" />
+      {
+        <form onSubmit={submitHandler} ref={formRef}>
+          <div className={classes.form}>
+            <div className={classes.form_fields}>
+              <label htmlFor="text">Company Ticker </label>
+              <input ref={tickerInputRef} type="text" placeholder="Ticker" />
+            </div>
+
+            <div className={classes.form_fields}>
+              <label htmlFor="price">Price</label>
+              <input ref={priceInputRef} type="number" placeholder="Price" />
+            </div>
+
+            <div className={classes.form_fields}>
+              <label htmlFor="quantity">Quantity</label>
+              <input
+                ref={quantityInputRef}
+                type="number"
+                placeholder="Shares"
+              />
+            </div>
           </div>
 
-          <div className={classes.form_fields}>
-            <label htmlFor="price">Price</label>
-            <input ref={priceInputRef} type="number" placeholder="Price" />
+          <div className={classes.btn_center}>
+            <Button>Add Transactions</Button>
           </div>
+        </form>
+      }
 
-          <div className={classes.form_fields}>
-            <label htmlFor="quantity">Quantity</label>
-            <input ref={quantityInputRef} type="number" placeholder="Shares" />
-          </div>
-        </div>
-
-        <div className={classes.btn_center}>
-          <Button>Add Transactions</Button>
-        </div>
-      </form>
+      {/* TODO: IMPLEMENT REUSABLE FORM */}
+      {/* <Form
+        submitHandler={submitHandler}
+        formRef={formRef}
+        tickerRef={tickerInputRef}
+        priceRef={priceInputRef}
+        quantityInputRef={quantityInputRef}
+        button={`Add Transaction`}
+      /> */}
     </div>
   );
 };
