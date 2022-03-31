b8ba92f4f438ecf6341b11d335198bfafd1ab6bb
Diego Quintela <diegolucasq@gmail.com>
2022-03-31 10:59:17 -0400

    Auth context

--- client/src/App.js
+++ client/src/App.js
@@ -21,13 +21,16 @@ const Articles = React.lazy(() => import('./articles/pages/Articles'));
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(null);
 
-  const login = useCallback(() => {
+  const login = useCallback((uid) => {
     setIsLoggedIn(true);
+    setUserId(uid);
   }, []);
 
   const logout = useCallback(() => {
     setIsLoggedIn(false);
+    setUserId(null);
   }, []);
 
   let routes;
@@ -60,7 +63,7 @@ function App() {
   }
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, userId, login, logout }}>
       <Router>
         <MainNavigation />
         {/* <Header /> */}
